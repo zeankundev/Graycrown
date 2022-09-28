27107042b80e5336dd073ed90c6ededcc03f1c84
zeankun.dev <zeanfender11@gmail.com>
2022-09-28 14:43:31 +0700

    Update child.js

--- script/child.js
+++ script/child.js
@@ -1,12 +1,5 @@
-const remote = require('@electron/remote/main');
 const params = new URLSearchParams(location.search)
-const res = params.get('url')
-const fs = require('fs');
-const notifier = require('node-notifier');
-const download = require('download');
-const { url } = require('inspector');
 const webView = document.getElementById('webview');
-const { webViewTag } = require('@electron/remote/main');
 const title = document.getElementById('title');
 const close = document.getElementById('close');
 const minimize = document.getElementById('min');
@@ -17,27 +10,11 @@ const refresh = document.getElementById('refresh');
 const downloads = document.getElementById('downloads');
 const titleText = document.getElementById('title-text')
 const imageToggle = document.getElementById('img-toggle');
-const getWin = () => remote.BrowserWindow.getFocusedWindow();
-var logic = 0;
 window.onload = function() {
     webView.setAttribute('src', 'https://' + res)
     var url = webView.src;
     title.innerHTML = url
 }
-const closeWin = () => {
-    getWin().close();
-}
-const minimizeWin = () => {
-    getWin().minimize();
-}
-const maximizeWin = () => {
-    const win = getWin();
-    win.isMaximized() ? win.unmaximize() : win.maximize();
-    win.isMaximized() ? imageToggle.setAttribute("src", "../assets/restore_down_1024.png") : imageToggle.setAttribute("src", "../assets/maximize_1024.png")
-}
-minimize.addEventListener('click', minimizeWin);
-maximize.addEventListener('click', maximizeWin);
-close.addEventListener('click', closeWin);
 back.addEventListener('click', function(e) {
     e.preventDefault();
     webView.goBack();
