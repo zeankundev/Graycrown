e221bc4d1999ee67042e395ce1933e718b5a01ee
zeankun.dev <zeanfender11@gmail.com>
2023-01-02 11:03:04 +0700

    Add files via upload

---
+++ docs/js/hub.js
@@ -0,0 +1,27 @@
+const params = new URLSearchParams(location.search)
+const query = params.get('style');
+let res;
+let hubRes
+fetch('../host/stylehub.json')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    res = data.hubs;
+    hubRes = res.find(hub => hub.name === query)
+    document.getElementById('name-pub').innerHTML = `${hubRes.name} by ${hubRes.publisher}`
+    document.getElementById('desc').innerHTML = hubRes.description;
+    document.getElementById('preview').src = `../assets/stylehub/preview.html?style=${hubRes.link}`
+    document.getElementById('down').onclick = () => {
+        var link = document.createElement("a");
+        // If you don't know the name or want to use
+        // the webserver default set name = ''
+        link.setAttribute('download', hubRes.link);
+        link.href = hubRes.link;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+})
+.catch(e => {
+     console.log("normal: " + e);
+})
---
+++ docs/js/stylehub.js
@@ -0,0 +1,21 @@
+fetch('../host/stylehub.json')
+.then(res => res.json())
+.then(data => {
+    let styleLib = document.getElementById('style-lib')
+    styleLib.innerHTML = '';
+    data.hubs.forEach(hub => {
+        let display = document.createElement('div')
+        display.className = 'display';
+        display.setAttribute('style', `background: url('${hub.img}') no-repeat; background-size: cover;`)
+        display.title = hub.description;
+        display.innerHTML = `<h2>${hub.name}</h2>&nbsp;by ${hub.publisher}&nbsp;<br>`
+        let redirButton = document.createElement('button')
+        redirButton.className = 'redir';
+        redirButton.innerHTML = 'Go to CSS info';
+        redirButton.onclick = () => {
+            window.location.href = `details.html?style=${hub.name}`
+        }
+        display.appendChild(redirButton)
+        styleLib.appendChild(display)
+    })
+})
